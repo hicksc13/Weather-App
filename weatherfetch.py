34648e3265e1893ad84b2ded671f2adde34c17e7
Cody Hicks <codytyler.hicks@outlook.com>
2022-12-01 14:31:47 -0600

    Adding Small code
    API test for news and Weather API

---
+++ weatherfetch.py
@@ -0,0 +1,20 @@
+import requests
+
+API_KEY = "83536a95d4dd86b3883578912f0f2d0f"
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+city = input("Enter a city name: ")
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+response = requests.get(request_url)
+
+
+if response.status_code == 200:
+    data = response.json()
+    weather = data['weather'][0]['description']
+    temperaturek = round(data["main"]["temp"] - 273.15, 2)
+    temperature = round(temperaturek*1.8+32, 2)
+
+    print("Weather:", weather)
+    print("Temperature:",temperature, "Fahrenheit")
+else:
+    print("An error has occurred.")
