ffe270fc9a10ea5d20fbb580322ad3cbb3e56337
Cody Hicks <115333868+hicksc13@users.noreply.github.com>
2022-12-02 12:15:48 -0600

    day17

---
+++ 100DOC/16/main.py
@@ -0,0 +1,26 @@
+#import turtle
+#timmy = turtle.Turtle()
+
+
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("red")
+# timmy.shapesize(5)
+# timmy.back(500)
+
+
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+
+# from prettytable import PrettyTable
+# table = PrettyTable()
+
+# table.add_column("PokemonName", ["Pikachu", "Squirtle", "Charmander"])
+# table.add_column("Type", ["Electric","Water", "Fire"])
+# table.align = "l"
+# print(table.align)
+# print(table)
