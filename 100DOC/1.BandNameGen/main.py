e3350c0a667e4101104f2f2d92267fd5e80620c2
Cody Hicks <115333868+hicksc13@users.noreply.github.com>
2023-01-03 00:02:50 -0600

    Moving Files out that arent ready to be shown. Preping for having Git hub Public

--- 100DOC/1.BandNameGen/main.py
+++
@@ -1,5 +0,0 @@
-print("Welcome to the band name generator!")
-x = input("Whats the name of the city you grew up in?: ")
-y = input("What is your pets name?: ")
-print("Great your band name will be " + x + " " + y)
-
