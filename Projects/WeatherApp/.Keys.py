7317f7682fd9aa1eaf9ffaf5af96d3a50351816d
Cody Hicks <115333868+hicksc13@users.noreply.github.com>
2023-01-03 17:20:20 -0600

    Removed personal API

---
+++ Projects/WeatherApp/.Keys.py
@@ -0,0 +1,2 @@
+API_KEY = "83536a95d4dd86b3883578912f0f2d0f"
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
--- Projects/WeatherApp/WeatherApp.py
+++ Projects/WeatherApp/WeatherApp.py
@@ -2,8 +2,9 @@ import tkinter
 import customtkinter
 import requests
 
-API_KEY = "83536a95d4dd86b3883578912f0f2d0f"
-BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+### CHANGE API KEY #####
+API_KEY = ""
+BASE_URL = ""
 
 def search():
     
